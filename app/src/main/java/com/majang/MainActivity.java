aead0ce138d3553e4bda043825e98a6ec0a2f186
Silver_Hsiao <silver041692@gmail.com>
2022-12-23 16:22:37 +0800

    修改比較器

--- app/src/main/java/com/majang/MainActivity.java
+++ app/src/main/java/com/majang/MainActivity.java
@@ -75,25 +75,16 @@ class ChineseNumberUtil {
     public static int chineseNumber2Int(String chineseNumber){
         int result = 0;
         int temp = 1;
-        int count = 0;
         char[] cnArr = new char[]{'一','二','三','四','五','六','七','八','九'};
         for (int i = 0; i < chineseNumber.length(); i++) {
-            boolean b = true;
             char c = chineseNumber.charAt(i);
             for (int j = 0; j < cnArr.length; j++) {
                 if (c == cnArr[j]) {
-                    if(0 != count){
-                        result += temp;
-                        temp = 1;
-                        count = 0;
-                    }
-
                     temp = j + 1;
-                    b = false;
                     break;
                 }
             }
-            if (i == chineseNumber.length() - 1) {//遍历到最后一个字符
+            if (i == chineseNumber.length() - 1) {
                 result += temp;
             }
         }
