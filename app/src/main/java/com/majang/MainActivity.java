e9a4389078590e2779a12b7296d09d6d32679226
Silver_Hsiao <silver041692@gmail.com>
2022-12-27 23:34:21 +0800

    完成四類牌顯示

--- app/src/main/java/com/majang/MainActivity.java
+++ app/src/main/java/com/majang/MainActivity.java
@@ -47,34 +47,46 @@ public class MainActivity extends AppCompatActivity {
 
         try{
             for(String[] s : majang_list){
-                View tiao_item = getLayoutInflater().inflate(R.layout.majang_item, linearLayout, false);
-                ImageView imageView = tiao_item.findViewById(R.id.majang_icon);
-                TextView textView = tiao_item.findViewById(R.id.majang_name);
+                View majang_item = getLayoutInflater().inflate(R.layout.majang_item, linearLayout, false);
+                ImageView imageView = majang_item.findViewById(R.id.majang_icon);
+                TextView textView = majang_item.findViewById(R.id.majang_name);
                 InputStream inputStream = getAssets().open(String.format("majang_icon/%s/%s", s[0], s[1]));
                 imageView.setImageDrawable(Drawable.createFromStream(inputStream, null));
                 imageView.requestLayout();
                 imageView.getLayoutParams().height = 100;
                 imageView.getLayoutParams().width = 100;
                 textView.setText(s[1]);
-                tiao_item.setTag("unselected");
-                tiao_item.setPadding(10, 10, 10, 10);
-                tiao_item.setOnClickListener(new View.OnClickListener() {
+                majang_item.setTag("unselected");
+                majang_item.setPadding(10, 10, 10, 10);
+                majang_item.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         //點擊後圖片向上移動
-                        if (tiao_item.getTag().equals("selected")) {
+                        if (majang_item.getTag().equals("selected")) {
                             //remove stroke
-                            tiao_item.setBackgroundResource(0);
-                            tiao_item.setTag("unselected");
+                            majang_item.setBackgroundResource(0);
+                            majang_item.setTag("unselected");
                         } else {
                             //set out stroke
-                            tiao_item.setBackgroundResource(R.drawable.majang_item_stroke);
-                            tiao_item.setTag("selected");
+                            majang_item.setBackgroundResource(R.drawable.majang_item_stroke);
+                            majang_item.setTag("selected");
                         }
                     }
                 });
                 if (s[0].equals("tiao")){
-                    linearLayout.addView(tiao_item);
+                    linearLayout.addView(majang_item);
+                }
+                if (s[0].equals("wan")){
+                    LinearLayout linearLayout1 = findViewById(R.id.wan_linear_layout);
+                    linearLayout1.addView(majang_item);
+                }
+                if (s[0].equals("tong")){
+                    LinearLayout linearLayout1 = findViewById(R.id.tong_linear_layout);
+                    linearLayout1.addView(majang_item);
+                }
+                if (s[0].equals("zi")){
+                    LinearLayout linearLayout1 = findViewById(R.id.zi_linear_layout);
+                    linearLayout1.addView(majang_item);
                 }
             }
         }catch (IOException e){
@@ -93,17 +105,26 @@ class ChineseNumberUtil {
         int result = 0;
         int temp = 1;
         char[] cnArr = new char[]{'一','二','三','四','五','六','七','八','九'};
-        for (int i = 0; i < chineseNumber.length(); i++) {
+        char[] ziArr = new char[]{'北','西','南','東','白','發','中'};
+        for(int i = 0; i < chineseNumber.length(); i++){
             char c = chineseNumber.charAt(i);
-            for (int j = 0; j < cnArr.length; j++) {
-                if (c == cnArr[j]) {
+            boolean isNumber = false;
+            for(int j = 0; j < cnArr.length; j++){
+                if(c == cnArr[j]){
                     temp = j + 1;
+                    isNumber = true;
                     break;
                 }
             }
-            if (i == chineseNumber.length() - 1) {
-                result += temp;
+            if(!isNumber){
+                for(int j = 0; j < ziArr.length; j++){
+                    if(c == ziArr[j]){
+                        temp = j + 1;
+                        break;
+                    }
+                }
             }
+            result = result * 10 + temp;
         }
         return result;
     }
