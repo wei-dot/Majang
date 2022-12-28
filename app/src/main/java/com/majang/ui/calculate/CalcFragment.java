c78594ef2198d2184011f351a55695681b87f89e
wtbigak <walton6180831@gmail.com>
2022-12-29 05:05:15 +0800

    符數計算機功能OK

--- app/src/main/java/com/majang/ui/calculate/CalcFragment.java
+++ app/src/main/java/com/majang/ui/calculate/CalcFragment.java
@@ -6,27 +6,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.majang.Adapter.FuiRecyclerViewAdapter;
+import com.majang.Constants;
 import com.majang.Hai;
+import com.majang.R;
 import com.majang.databinding.FragmentCalcBinding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CalcFragment extends Fragment {
 
     private FragmentCalcBinding binding;
-    protected RecyclerView.LayoutManager mLayoutManager;
-    protected FuiRecyclerViewAdapter mAdapter;
-    protected RecyclerView mRecyclerView;
-    protected ArrayList<Integer> mDataset = new ArrayList<>();
+
 
 
     @Override
@@ -38,10 +39,9 @@ public class CalcFragment extends Fragment {
         binding = FragmentCalcBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         binding.calcButton.setOnClickListener(v -> {
-            int fan = Integer.parseInt(Objects.requireNonNull(binding.textInputLayout.getEditText()).getText().toString());
-            Log.d("fan", String.valueOf(fan));
-            int[] ans = Hai.fuCalculate(40, fan);
-            Log.d("ans", String.format("%d %d", ans[0], ans[1]));
+            int fan = Integer.parseInt(Objects.requireNonNull(binding.fanInput.getEditText()).getText().toString());
+            int fu = Integer.parseInt(binding.filledExposedDropdown.getText().toString());
+            int[] ans = Hai.fuCalculate(fu, fan);
             new AlertDialog.Builder(getContext())
                     .setTitle("計算結果")
                     .setMessage(String.format("莊家得點: %d\n閒家得點: %d", ans[0], ans[1]))
@@ -49,6 +49,11 @@ public class CalcFragment extends Fragment {
                     .show();
 
         });
+        ArrayList<Integer> fui = Arrays.stream(Constants.fui).boxed().collect(Collectors.toCollection(ArrayList::new));
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, fui);
+        binding.filledExposedDropdown.setText("20", false);
+        binding.filledExposedDropdown.setAdapter(adapter);
+        binding.filledExposedDropdown.setDropDownBackgroundResource(R.color.white);
         return root;
     }
 
